c4f8ab8c8461ce4be5499f311f4c31841f5768fd
Pablo Zapata <tamtok2.0@gmail.com>
2025-03-10 21:27:06 -0600

    Navbar 10/03/25

--- OxxoPage/wwwroot/js/site.js
+++ OxxoPage/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function updateDateTime() {
+    const now = new Date();
+    const optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const optionsTime = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    document.getElementById('datetime').textContent = now.toLocaleTimeString('es-ES', optionsTime) + '  -  ' + now.toLocaleDateString('es-ES', optionsDate);
+}
+setInterval(updateDateTime, 1000);
+updateDateTime();
