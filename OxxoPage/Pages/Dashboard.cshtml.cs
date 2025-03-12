1268cf8317f38c55d30321bfd29bb0cf4521118e
Pablo Zapata <tamtok2.0@gmail.com>
2025-03-11 22:35:24 -0600

    Home Final FrontEnd

---
+++ OxxoPage/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OxxoPage.Pages;
+
+public class Dashboard : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
