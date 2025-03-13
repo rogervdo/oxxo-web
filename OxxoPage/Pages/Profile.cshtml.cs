0c7b4947d6e8348935a2ed508654d24d9979b8b2
Pablo Zapata <tamtok2.0@gmail.com>
2025-03-12 22:26:13 -0600

    Profile_Final

--- OxxoPage/Pages/Profile.cshtml.cs
+++ OxxoPage/Pages/Profile.cshtml.cs
@@ -8,19 +8,19 @@ public class Profile : PageModel
     public string UserName { get; set; } = "Juan Pérez";
     public string Role { get; set; } = "Asesor de Tienda";
     public int Level { get; set; } = 2;
-    public int CurrentXP { get; set; } = 5200;
-    public int RequiredXP { get; set; } = 6000;
+    public int CurrentXP { get; set; } = 50;
+    public int RequiredXP { get; set; } = 100;
     public double Progress => (double)CurrentXP / RequiredXP * 100;
-    public string Biography { get; set; } = "Texto de biografía...";
+    public string Biography { get; set; } = "HOLA SOY PEPITO";    
     public string ProfileImageUrl { get; set; } = "perfil.jpg";
     
     public List<Achievement> Achievements { get; set; } = new()
     {
-        new Achievement("Certificación Oro en Administración de Tienda", new DateTime(2025, 2, 20), 5, 5),
-        new Achievement("Mantenimiento", new DateTime(2025, 2, 18), 5, 10),
-        new Achievement("Bronce en Planometría", new DateTime(2025, 2, 17), 8, 10),
-        new Achievement("Plata en Reabastecimiento", new DateTime(2025, 2, 14), 9, 10),
-        new Achievement("Oro en Drive-Thru", new DateTime(2025, 2, 10), 10, 10),
+        new Achievement("Certificación Oro en Administración de Tienda", new DateTime(2025, 2, 20), 5),
+        new Achievement("Mantenimiento", new DateTime(2025, 2, 18), 5),
+        new Achievement("Bronce en Planometría", new DateTime(2025, 2, 17), 8),
+        new Achievement("Plata en Reabastecimiento", new DateTime(2025, 2, 14), 9),
+        new Achievement("Oro en Drive-Thru", new DateTime(2025, 2, 10), 10),
     };
 }
 
@@ -30,13 +30,13 @@ public class Achievement
     public DateTime Date { get; set; }
     public int Score { get; set; }
     public int TotalCorrect { get; set; }
+    public string IconPath { get; set; } = "medal.png";
 
-    public Achievement(string title, DateTime date, int score, int totalCorrect)
+    public Achievement(string title, DateTime date, int score)
     {
         Title = title;
         Date = date;
         Score = score;
-        TotalCorrect = totalCorrect;
     }
 }
 
