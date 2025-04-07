fde069344ae57ffa0d876bd772bd70d969ab16d3
4ll3ssandro <a00839934@tec.mx>
2025-04-06 20:05:49 -0600

    Estica de Settings y mitad de Appearance
    Falta agregar todo lo de base de datos, la estica de settings ya esta todo. En Appearance falta dos cosas; ademas falta el java script para todo.

---
+++ OxxoPage/Pages/Appearance.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using OxxoPage.Model;
+
+namespace OxxoPage.Pages
+{
+    public class Appearance : PageModel
+    {
+        public void OnGet(){
+            
+        }
+    }
+}    
